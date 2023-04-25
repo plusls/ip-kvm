cc42fe70d57229bbd8f72d576f01b23eab93c929
plusls <plusls@qq.com>
2023-04-25 16:43:15 +0800

    优化前端

--- ip-kvm-assets/index.ts
+++ ip-kvm-assets/index.ts
@@ -147,19 +147,21 @@ function resize_video() {
     img.height = window.innerHeight - 45;
 }
 
-let stream_url_input = document.getElementById("stream_url") as HTMLInputElement;
+function init_stream_url_input() {
+    let stream_url_input = document.getElementById("stream_url") as HTMLInputElement;
 
-stream_url_input.addEventListener("change", function (event: Event) {
-    let img: HTMLImageElement = document.getElementById("video") as HTMLImageElement;
-    let stream_url_input = event.target as HTMLInputElement;
-    img.src = stream_url_input.value;
-    localStorage.setItem('stream_url', JSON.stringify(stream_url_input.value));
-});
+    stream_url_input.addEventListener("change", function (event: Event) {
+        let img: HTMLImageElement = document.getElementById("video") as HTMLImageElement;
+        let stream_url_input = event.target as HTMLInputElement;
+        img.src = stream_url_input.value;
+        localStorage.setItem('stream_url', JSON.stringify(stream_url_input.value));
+    });
 
-if ('stream_url' in localStorage) {
-    stream_url_input.value = JSON.parse(localStorage.getItem('stream_url'));
-    let img: HTMLImageElement = document.getElementById("video") as HTMLImageElement;
-    img.src = stream_url_input.value;
+    if ('stream_url' in localStorage) {
+        stream_url_input.value = JSON.parse(localStorage.getItem('stream_url'));
+        let img: HTMLImageElement = document.getElementById("video") as HTMLImageElement;
+        img.src = stream_url_input.value;
+    }
 }
 
 
@@ -167,3 +169,4 @@ resize_video();
 
 document.addEventListener('keydown', on_key_down);
 document.addEventListener('keyup', on_key_up);
+init_stream_url_input();
